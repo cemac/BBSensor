c6bdd8c4b2ae02cf79696079b1bd410d3546009d
cemac-ccs <c.c.symonds@leeds.ac.uk>
2020-11-09 12:53:14 +0000

    Rolling back improper merge

--- serverpi/tests/__main__.py
+++ serverpi/tests/__main__.py
@@ -1,5 +1,5 @@
-''' 
-python3 -m sensorpi.tests
+'''
+python3 -m serverpi.tests
 '''
 
 import os,sys
@@ -7,7 +7,7 @@ print('This is pi of serial number: ',os.popen('cat /sys/firmware/devicetree/bas
 
 
 args = sys.argv[1:]
-if len(args) == 0 : args = 'interrupt db temp opc gps'.split()
+if len(args) == 0 : args = 'interrupt db opc'.split()
 
 
 from . import pyvers
@@ -15,13 +15,10 @@ if 'interrupt' in args:
   from . import interrupt_test
 if 'db' in args:
   from . import db_test
-  # 
 if 'temp' in args:
   from . import DHT_test
 if 'opc' in args:
   from . import opc_test
-if 'gps' in args:
-  from . import gps_test
 
 
 
